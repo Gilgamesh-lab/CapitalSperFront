131a69323d445e01dcc75e0bd808536d5cbffaec
Gilgamesh-lab <fabrice.amegadjen@gmail.com>
2024-08-13 20:02:28 +0200

    Correction

--- deploiement.bat
+++ deploiement.bat
@@ -3,5 +3,5 @@
   echo|set /p="<h1>Erreur 404, la page n'a pas pu être trouvé</h1>">>docs/404.html
 %@EndTry%
 :@Catch
-  echo erreur lors de l'exécution du .bat
+  echo Erreur lors de l'exécution du .bat
 :@EndCatch
