4de0e545af1b79dfc228bcbad6b28d50de2650ea
Gilgamesh-lab <fabrice.amegadjen@gmail.com>
2024-08-14 22:50:24 +0200

    Mise en place des routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ListePersonnageComponent } from './liste-personnage/liste-personnage.component';
+import { DetailPersonnageComponent } from './detail-personnage/detail-personnage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'personnages', component: ListePersonnageComponent},
+    {path: 'personnages/:id', component: DetailPersonnageComponent},
+    {path: '',  redirectTo: 'personnages', pathMatch: 'full' }
+];
