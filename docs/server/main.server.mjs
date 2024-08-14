ced6f6992ee4e012700d10fddca3d0ab475949c8
Gilgamesh-lab <fabrice.amegadjen@gmail.com>
2024-08-15 00:44:05 +0200

    Deploiement d'une nouvelle version

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-YETZFL35.mjs";import"./chunk-ZNIKK7OG.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-3DFY763G.mjs";import"./chunk-PWM3WBYT.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
