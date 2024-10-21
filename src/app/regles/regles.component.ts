8f7163c9c25e4856b5182c49ab83868968aff4ca
Gilgamesh-lab <fabrice.amegadjen@gmail.com>
2024-10-21 14:32:36 +0200

    Ajout ordre d'appel

--- src/app/regles/regles.component.ts
+++ src/app/regles/regles.component.ts
@@ -4,17 +4,20 @@ import { CARTES } from '../carte/mock-cartes-list';
 import { AuthService } from '../auth.service';
 import { CommonModule } from '@angular/common';
 import { carteService } from '../carte/carte.service';
+import { LoaderComponent } from '../carte/loader/loader.component';
+import { Carte } from '../carte/carte';
+import { CAMPS } from '../carte/mock-camps-list';
 
 @Component({
   selector: 'app-regles',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LoaderComponent],
   templateUrl: './regles.component.html',
   styleUrl: './regles.component.css'
 })
 export class ReglesComponent {
 
-  constructor(private router: Router, private auth: AuthService, private carteService: carteService){
+  constructor(private router: Router, private auth: AuthService, public carteService: carteService){
   
   }
 
@@ -27,6 +30,26 @@ export class ReglesComponent {
 
   goMenu(){
     this.router.navigate(['/']);
+  }
+
+  getCamps(idCamps){
+    return CAMPS.find((camp) => camp.id == idCamps);
+  }
+
+  estActiver(id: number): boolean{
+    return (CARTES.find((carte) => carte.id == id).estActiver || this.auth.isLoggedIn);
+  }
+
+  campActiver(idCamps: number): boolean{
+    return this.carteService.cartes.filter((carte) => carte.camps != null && carte.camps.id == idCamps ).length != 0;
+  }
+
+  getCarteNom(id: number): String{
+    return this.carteService.cartes.find((carte) => carte.id == id).nom;
+  }
+
+  getCarte(id: number): Carte{
+    return this.carteService.cartes.find((carte) => carte.id == id);
   }
 
   goCamps(id: number){
